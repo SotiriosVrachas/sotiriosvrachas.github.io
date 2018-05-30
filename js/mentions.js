78c1a1612074070d4f3f91c365f9c8d878e84e49
Sotirios Vrachas <sotirios@vrachas.com>
2018-05-30 03:44:43 +0000

    Update mentions.js

--- js/mentions.js
+++ js/mentions.js
@@ -1,17 +1,12 @@
 jQuery(function($){
-  var urls = [];
-  var base;
 
   $("*[data-webmention-count]").each(function(i,e){
     var parser = document.createElement('a');
-    parser.href = $(e).data('url');
-    base = parser.protocol + "//" + parser.hostname;
-    urls.push(parser.pathname+parser.search);
+    target = $(e).data('url');
   });
 
   $.getJSON("https://webmention.io/api/count?jsonp=?", {
-    base: base,
-    target: urls.join(",")
+    target: target
   }, function(data){
     $("*[data-webmention-count]").each(function(i,e){
       $(e).text(data.count);
