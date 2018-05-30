cb72829f0098c279c7a24bf14c0d1dc3df0c0fe0
Sotirios Vrachas <sotirios@vrachas.com>
2018-05-30 03:51:46 +0000

    Update mentions.js

--- js/mentions.js
+++ js/mentions.js
@@ -1,6 +1,6 @@
 jQuery(function($){
 
-  $("*[data-webmention-count]").each(function(i,e){
+  $("*[data-webmention]").each(function(i,e){
     var parser = document.createElement('a');
     target = $(e).data('url');
   });
@@ -12,4 +12,13 @@ jQuery(function($){
       $(e).text(data.count);
     });
   });
+  
+  $.getJSON("https://webmention.io/api/mentions?jsonp=?", {
+    target: target
+  }, function(data){
+    $("*[data-webmention]").each(function(i,e){
+      $(e).html(data.links[0].activity.sentence_html);
+    });
+  });
+  
 });
