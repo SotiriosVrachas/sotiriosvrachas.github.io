2d28f07c47c66dea9d01a459ebb867ded824b805
Sotirios Vrachas <sotirios@vrachas.com>
2018-05-30 02:10:20 +0000

    Create mentions.js

---
+++ js/mentions.js
@@ -0,0 +1,20 @@
+jQuery(function($){
+  var urls = [];
+  var base;
+
+  $("*[data-webmention-count]").each(function(i,e){
+    var parser = document.createElement('a');
+    parser.href = $(e).data('url');
+    base = parser.protocol + "//" + parser.hostname;
+    urls.push(parser.pathname+parser.search);
+  });
+
+  $.getJSON("https://webmention.io/api/count?jsonp=?", {
+    base: base,
+    target: urls.join(",")
+  }, function(data){
+    $("*[data-webmention-count]").each(function(i,e){
+      $(e).text(data.count[$(e).data('url')]);
+    });
+  });
+});
